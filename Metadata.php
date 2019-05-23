0457b599f1b8e32ee6eaea34357618409c91c0f8
lfs <schenonef@gmail.com>
2019-05-23 13:38:06 -0700

    Allow to set and get multiple values at once

--- Metadata.php
+++ Metadata.php
@@ -10,32 +10,53 @@ class Metadata {
 	}
 
 	/**
-	 * Set metadata
+	 * Set metadata for the given page
 	 */
-	static function set( $page, $key, $value ) {
+	static function set( int $page, $keyOrData, $value = null ) {
+		if ( is_array( $keyOrData ) ) {
+			$data = $keyOrData;
+		} else if ( is_string( $keyOrData ) ) {
+			$key = $keyOrData;
+			$data = [ $key => $value ];
+		} else {
+			return;
+		}
+
 		$dbw = wfGetDB( DB_MASTER );
-		return $dbw->upsert(
-			'metadata',
-			[
-				'md_page' => $page,
-				'md_key' => $key,
-				'md_value' => $value,
-			],
-			[
-				'md_page',
-				'md_key',
-			],
-			[
-				'md_value' => $value,
-			]
-		);
+		foreach ( $data as $key => $value ) {
+			$dbw->upsert(
+				'metadata',
+				[
+					'md_page' => $page,
+					'md_key' => $key,
+					'md_value' => $value,
+				],
+				[
+					'md_page',
+					'md_key',
+				],
+				[
+					'md_value' => $value,
+				]
+			);
+		}
 	}
 
 	/**
-	 * Get metadata
+	 * Get metadata for the given page and key
 	 */
-	static function get( $page, $key ) {
+	static function get( int $page, $key = null ) {
 		$dbr = wfGetDB( DB_REPLICA );
-		return $dbr->selectField( 'metadata', 'md_value', "md_page = '$page' AND md_key = '$key'" );
+
+		if ( $key ) {
+			return $dbr->selectField( 'metadata', 'md_value', "md_page = $page AND md_key = '$key'" );
+		}
+
+		$data = [];
+		$result = $dbr->select( 'metadata', 'md_key, md_value', "md_page = $page" );
+		foreach ( $result as $row ) {
+			$data[ $row->md_key ] = $row->md_value;
+		}
+		return $data;
 	}
 }
